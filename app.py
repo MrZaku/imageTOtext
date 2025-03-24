e23f114a44c784a77c4efbed790931d2687614e7
MrZaku <110213251+MrZaku@users.noreply.github.com>
2025-03-24 11:30:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def extract_text():
     return render_template('index.html', extracted_text=extracted_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
